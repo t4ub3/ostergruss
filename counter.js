e449c3691ea4bc4e894d69c1dcc0cfb74ce317f9
JuliaNeumann <jn.julianeumann@googlemail.com>
2020-04-11 14:02:17 +0200

    finished

--- counter.js
+++ counter.js
@@ -1,33 +1,110 @@
 let eggs = {
+  scroll: {
+    found: false,
+    text:
+      "Bei uns ist ALLE Tage im Jahr Ostern, nur dass man einmal im Jahr Ostern feiert. (Martin Luther)",
+  },
   hover: {
     found: false,
-    text: "hover egg",
+    text: "What's up, Doc?",
   },
   link: {
     found: false,
-    text: "link egg",
+    text: "Linke Tür, rechts einordnen, jeder nur ein Kreuz!",
+  },
+  image: {
+    found: false,
+    text: "Ich schenke dir ein Osterei, wenn du's zerbrichst, so hast du zwei.",
+  },
+  print: {
+    found: false,
+    text: "Jede Liebesgeschichte erzählt von Ostern. (Monika Minder)",
+  },
+  isbn: {
+    found: false,
+    text: "Wer Ostern kennt, kann nicht verzweifeln. (Dietrich Bonhoeffer)",
+  },
+  form: {
+    found: false,
+    text:
+      "Es ist das Osterfest alljährlich für den Hasen recht beschwerlich. (Wilhem Busch)",
+  },
+  egg: {
+    found: false,
+    text:
+      "Das weiß ein jeder, wer's auch sei, gesund und stärkend ist das Ei. (Wilhelm Busch)",
+  },
+  select: {
+    found: false,
+    text:
+      "Ostern ist die Zukunft des Menschen. Hier wird er wieder der Mensch, wie er geplant war. (August Everding)",
+  },
+  resize: {
+    found: false,
+    text: "Nu sajaz, nu pogodi!",
   },
 };
 
+let intro =
+  "Du hast es vielleicht schon geahnt... auch auf dieser Seite gibt es 10 Easter Eggs :) Du hast gerade eins gefunden. Wenn du unten rechts auf das Ei klickst, siehst du, welche du schon gefunden hast. Viel Spaß!<br><br>";
 let counter = 0;
 let countDisplay = document.getElementById("count");
+let countWrapper = document.getElementById("count-wrapper");
+let eggAlert = document.getElementById("egg-alert");
+let eggAlertText = document.getElementById("egg-alert-text");
+let eggList = document.getElementById("egg-list");
+let eggListTexts = document.getElementById("egg-list-texts");
+let eggListCount = document.getElementById("egg-list-count");
 
-function count(id, event) {
+function findEgg(id, event) {
   let egg = eggs[id];
   if (!egg.found) {
     if (event) {
       event.preventDefault();
     }
     egg.found = true;
-    alert(egg.text);
     counter++;
     setCountDisplay();
+    let text = getFoundEggsKeys().length === 1 ? intro : "";
+    alertEgg(text + egg.text);
   }
 }
 
 function setCountDisplay() {
   if (counter === 1) {
-    document.getElementById("count-wrapper").style.display = "block";
+    countWrapper.style.display = "block";
   }
   countDisplay.textContent = counter;
 }
+
+function alertEgg(text) {
+  eggList.style.display = "none";
+  eggAlertText.innerHTML = text;
+  eggAlert.style.display = "block";
+}
+
+document.getElementById("egg-alert-close").addEventListener("click", () => {
+  eggAlert.style.display = "none";
+});
+
+countWrapper.addEventListener("click", () => {
+  eggAlert.style.display = "none";
+  eggList.style.display = "block";
+  let eggListCountEggs = 0;
+  getFoundEggsKeys().forEach((key) => {
+    let li = document.createElement("li");
+    li.textContent = eggs[key].text;
+    eggListTexts.appendChild(li);
+    eggListCountEggs++;
+  });
+  eggListCount.textContent = eggListCountEggs;
+});
+
+function getFoundEggsKeys() {
+  return Object.keys(eggs).filter((key) => eggs[key].found);
+}
+
+document.getElementById("egg-list-close").addEventListener("click", () => {
+  eggList.style.display = "none";
+  eggListTexts.innerHTML = "";
+});
--- eggs.js
+++ eggs.js
@@ -1,7 +1,98 @@
+// --- HOVER
 document.getElementById("egg-hover").addEventListener("mouseover", () => {
-  count("hover");
+  findEgg("hover");
 });
 
+// --- LINK
 document.getElementById("egg-link").addEventListener("click", (event) => {
-  count("link", event);
+  findEgg("link", event);
+});
+
+// --- PRINT
+window.addEventListener("afterprint", () => {
+  findEgg("print");
+});
+
+// --- SCROLL
+let initialScroll = true;
+window.addEventListener("scroll", () => {
+  if (initialScroll) {
+    initialScroll = false;
+  } else {
+    findEgg("scroll");
+  }
+});
+
+// --- RESIZE
+window.addEventListener("resize", () => {
+  findEgg("resize");
+});
+
+// --- IMAGE
+let dragImg = document.getElementById("egg-img");
+let dropImg = document.getElementById("egg-drop");
+dragImg.addEventListener("dragstart", (event) => {
+  document
+    .getElementById("egg-img-wrapper")
+    .classList.add("egg-img-wrapper--expanded");
+  dropImg.style.display = "block";
+  event.dataTransfer.setData("text", "egg");
+});
+
+dropImg.addEventListener("dragover", (event) => {
+  event.preventDefault();
+});
+
+dropImg.addEventListener("drop", (event) => {
+  event.preventDefault();
+  if (event.dataTransfer.getData("text") === "egg") {
+    dragImg.style.left = 0;
+    dragImg.style.right = "auto";
+    dragImg.draggable = false;
+    findEgg("image");
+  }
+});
+
+// --- ISBN
+let typedText = "";
+let typeTimeout = null;
+window.addEventListener("keyup", (event) => {
+  typedText += event.key;
+  clearTimeout(typeTimeout);
+  typeTimeout = setTimeout(() => {
+    typedText = "";
+  }, 3000);
+  if (typedText === "-14032") {
+    clearTimeout(typeTimeout);
+    findEgg("isbn");
+  }
+});
+
+// --- FORM
+let searchInput = document.getElementById("search-input");
+document.getElementById("search").addEventListener("submit", (event) => {
+  event.preventDefault();
+  let searchText = searchInput.value.toLowerCase();
+  if (searchText === "easter egg" || searchText === "easteregg") {
+    findEgg("form");
+  } else {
+    window.open("https://de.wikipedia.org/wiki/" + searchText, "_blank");
+  }
+});
+
+// --- EGG
+document.getElementById("egg-alert-egg").addEventListener("click", () => {
+  findEgg("egg");
+});
+
+// --- SELECT
+document.addEventListener("mouseup", () => {
+  if (
+    window
+      .getSelection()
+      .toString()
+      .match(/^.{0,10}Egggs.{0,10}$/)
+  ) {
+    findEgg("select");
+  }
 });
